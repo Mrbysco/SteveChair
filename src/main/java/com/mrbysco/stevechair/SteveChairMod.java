881cc83438e413c44f82f1f200e1fceeca990e10
Mrbysco <Mrbysco@users.noreply.github.com>
2024-11-07 17:42:05 +0100

    Add Steve Chair to building blocks tab

--- src/main/java/com/mrbysco/stevechair/SteveChairMod.java
+++ src/main/java/com/mrbysco/stevechair/SteveChairMod.java
@@ -3,10 +3,12 @@ package com.mrbysco.stevechair;
 import com.mojang.logging.LogUtils;
 import com.mrbysco.stevechair.client.ClientHandler;
 import com.mrbysco.stevechair.registry.ChairRegistry;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import org.slf4j.Logger;
 
 @Mod(SteveChairMod.MOD_ID)
@@ -19,8 +21,16 @@ public class SteveChairMod {
 		ChairRegistry.BLOCK_ENTITIES.register(eventBus);
 		ChairRegistry.ITEMS.register(eventBus);
 
+		eventBus.addListener(this::addTabContents);
+
 		if (dist.isClient()) {
 			eventBus.addListener(ClientHandler::registerEntityRenders);
+		}
+	}
+
+	private void addTabContents(final BuildCreativeModeTabContentsEvent event) {
+		if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+			event.accept(ChairRegistry.STEVE_CHAIR);
 		}
 	}
 }
